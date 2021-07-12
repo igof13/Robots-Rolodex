7a4424b9ca8a62ae8195ec3303f0fbcc88737805
igor fugagnoli <igof13@gmail.com>
2021-07-12 14:33:10 -0300

    Updates

---
+++ precache-manifest.2e3ebe1baaaf3b5ee580d626385cf459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be2acad18aaa858b4f4a461d5f8d9295",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "b066e0a98c39928d8db7",
+    "url": "/robots-rolodex/static/css/main.d101e8c8.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/robots-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b066e0a98c39928d8db7",
+    "url": "/robots-rolodex/static/js/main.1d6acaa5.chunk.js"
+  },
+  {
+    "revision": "02542f5a8b44e87af7b4",
+    "url": "/robots-rolodex/static/js/runtime-main.07f6b5a4.js"
+  }
+]);
